8cc4463bd3f4c1e2e026b67834ea22d730973abf
SalmaAzeem <s-salma.azeem@zewailcity.edu.eg>
2023-12-14 10:57:37 +0200

    adding mom, ingredients json file + ingredient class modifications

--- Classes/Ingredinet.py
+++ Classes/Ingredinet.py
@@ -1,11 +1,26 @@
 import sqlite3
+import json
+
+def from_json_to_sql(path, id):
+    try:
+        with open(path, "r") as infile:
+            data = json.load(infile)
+            for ingredient in data:
+                if ingredient.get("Ingredient ID") == id:
+                    return ingredient.get("Ingredient")
+            return f"No ingredient found with this id: {id}"
+    except FileNotFoundError:
+        return f"File not found: {path}"
+    except json.JSONDecodeError:
+        return f"Error decoding json in file: {path}"
+
 
 class Ingredients:
-    def __init__(self, Ingredient_Name, Ingredient_Category):
-        self.Ingredient_Name = Ingredient_Name
-        self.Ingredient_Category = Ingredient_Category
+    def __init__(self, ingredient_name, ingredient_category):
+        self.ingredient_name = ingredient_name
+        self.ingredient_category = ingredient_category
 
-    def Get_Ingredient_from_database(database_name):
+    def get_ingredient_from_database(self, database_name):
         connection = sqlite3.connect(database_name)
         cursor = connection.cursor()
         data = cursor.execute('select * from Ingredient') 
@@ -15,10 +30,15 @@ class Ingredients:
         connection.commit()
         connection.close()
 
-    def Delete_Ingredient(connection, id):
+    def delete_ingredient(self, connection, id):
         cursor = connection.cursor()
         cursor.execute('delete from Ingredient where Ingredient_id = ?', (id))
         connection.commit()
 
-g = Ingredient("garlic", "veg")
-g.Get_Ingredient_from_database("Tasty.bd")
+Ingredient_object = Ingredients('Salt', 'Seasoning')
+database_name = "Tasty.db"
+Ingredient_object.Get_Ingredient_from_database("Tasty.db")
+connection = sqlite3.connect(database_name)
+Ingredient_object.Delete_Ingredient(connectio, 1)
+connection.close()
+
---
+++ Get_Ingredients.py
@@ -0,0 +1,35 @@
+import requests
+import json
+
+APP_ID = 'df476c39'
+APP_KEY = '879f8b246313c4c27dcf09a547c1b393'
+ENDPOINT_URL = 'https://api.edamam.com/api/recipes/v2'
+
+params = {
+    'q': 'chicken',
+    'app_id': APP_ID,
+    'app_key': APP_KEY,
+    'type': 'public'
+}
+
+response = requests.get(ENDPOINT_URL, params=params)
+
+if response.status_code == 200:
+    data = response.json()
+    ingredients_list = []
+    ingredient_id = 1
+    for hit in data['hits']:
+        for recipe in hit['recipe']['ingredients']:
+            ingredient_name = recipe['food']
+            ingredients_list.append({
+                "Ingredient Name: ": ingredient_name,
+                "Ingredient ID: " : ingredient_id
+            })
+            ingredient_id += 1
+            
+
+json_object = json.dumps(ingredients_list, indent = 1)
+with open("ingredients.json", "w") as outfile:
+    outfile.write(json_object)
+
+print('done')
