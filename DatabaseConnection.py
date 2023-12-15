514d1cb880f59cd66d8cd5371ff123da83305f05
shahdhoss <jessiecutie994@gmail.com>
2023-12-16 01:41:32 +0200

    the pantry class and added the vegetables json file containing recipe names and their ingredients

--- DatabaseConnection.py
+++ DatabaseConnection.py
@@ -5,7 +5,7 @@ import pdb
 connection = sqlite3.connect("RecipesDB")                                  #add the database name as it is saved on ur laptop
 cursor = connection.cursor() 
 
-with open('F:\downloads\cake_queryIngredients.json', 'r') as json_file:     #add ur own json file path
+with open('F:\downloads\ish_queryIngredients.json', 'r') as json_file:     #add ur own json file path
     file = json.load(json_file)
 
 # to insert into the recipes table using the json file
@@ -16,20 +16,20 @@ def insert_recipes_intotable(file):
             connection.execute("insert into Recipes (Recipe_name, Ingredient) VALUES (?, ?)", (str(recipe),str(ingredientt)))
             connection.commit()
 
-# #use when testing whether the database got the data or not::
-# cursor.execute("select * from Recipes")       
-# mydata=cursor.fetchall()                   #fetch all function gets whats stored in the database
-# for i in mydata:             
-#     print(i)
+# use when testing whether the database got the data or not::
+cursor.execute("select * from Recipes")       
+mydata=cursor.fetchall()                   #fetch all function gets whats stored in the database
+for i in mydata:             
+    print(i)
 
-def get_recipeInfo(RecipeN):
-        cursor.execute("Select Ingredient from Recipes where Recipe_name = ?", ([RecipeN]))
-        ingredients=cursor.fetchall()
-        return ingredients
+# def get_recipeInfo(RecipeN):
+#         cursor.execute("Select Ingredient from Recipes where Recipe_name = ?", ([RecipeN]))
+#         ingredients=cursor.fetchall()
+#         return ingredients
 
-ingredientss=get_recipeInfo("Meat Loaf")    #choose the recipe that u want to get the ingredients for
-for ingredient in ingredientss:
-    print(ingredient)
+# ingredientss=get_recipeInfo("Meat Loaf")    #choose the recipe that u want to get the ingredients for
+# for ingredient in ingredientss:
+#     print(ingredient)
 
 connection.close()
 
