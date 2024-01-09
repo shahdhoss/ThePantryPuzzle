66e6e468e34f99f1aeb6474b30e80634d8bc0581
SalmaAzeem <s-salma.azeem@zewailcity.edu.eg>
2024-01-09 17:19:52 +0200

    altered reviews table

--- add_all_to_main_db/add_reviews.py
+++ add_all_to_main_db/add_reviews.py
@@ -2,17 +2,17 @@ import sqlite3
 
 create_table = """
     create table Reviews(
-        User_ID int,
-        comment varchar(255)
+        User_ID varchar(50),
+        comment varchar(255),
+        Recipe_Name varchar(50)
     )
 """
 drop_table = "drop table Reviews"
 
 add_recipe = "alter table Reviews add Recipe_Name varchar(50)"
-connection = sqlite3.Connection("instance\MainDB.db")
+connection = sqlite3.Connection("ThePantryPuzzle\\instance\\MainDB.db")
 cursor = connection.cursor()
 
-cursor.execute(add_recipe)
-
+cursor.execute(create_table)
 connection.commit()
 connection.close()
