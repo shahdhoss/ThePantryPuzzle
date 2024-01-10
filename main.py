8b0300141c886a9f07a44d12468f9bac4650ab45
Mohaned <s-mohaned.hassan@zewailcity.edu.eg>
2024-01-10 22:00:24 +0200

    organized some files

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from backend.complete import create_app
-from os import urandom
 
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=int("3000"), debug=True)
+    app.run(host="0.0.0.0", port=3000, debug=True)
