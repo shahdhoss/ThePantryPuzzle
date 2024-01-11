ca3062de6046d6b8bed0259ca1459a8d2c7a7eb4
Malak Elbanna <67643605+malak-elbanna@users.noreply.github.com>
2024-01-11 10:52:25 +0200

    Update add_nutrients.py

--- add_all_to_main_db/add_nutrients.py
+++ add_all_to_main_db/add_nutrients.py
@@ -1,6 +1,6 @@
 import sqlite3
 
-connection = sqlite3.connect("instance\MainDB.db")
+connection = sqlite3.connect("instance/MainDB.db")
 cursor = connection.cursor()
 table_nutrients = 'create table Nutrients(Ingredient_name varchar(255), Nutrients varchar(255))'
 #cursor.execute(table_nutrients)
